5fc2e9f6be7d90e562d362bfe37200b732c88fac
dedejamaludin <96928337+dedejamaludinmuslim@users.noreply.github.com>
2025-12-09 02:49:19 +0700

    Update app.js

--- app.js
+++ app.js
@@ -941,14 +941,4 @@
       loadTrend(kode);
     });
   }
-
-    window.addEventListener("beforeinstallprompt", (e) => {
-    console.log("🔥 beforeinstallprompt fired", e);  // <-- tambah ini
-    e.preventDefault();
-    deferredPrompt = e;
-    if (btnInstallApp) {
-      btnInstallApp.classList.remove("hidden");
-    }
-  });
-
 })();
